d04adf9512b5cdbbe49c1a7d5721d2c62af9f709
Owlafiss <zdraveskia0@gmail.com>
2020-07-10 04:22:06 +0300

    third commit with time changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import ReacDOM from 'react-dom';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import moment from 'moment';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
@@ -8,19 +12,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Current Time is:<br></br>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Your time +30 mins:<br></br>
+          {
+            moment().add(30, 'minutes').calendar()
+          }
+          <br></br>Naverland 9 2600
+        </p>
+      
       </header>
     </div>
   );
 }
-
 export default App;
