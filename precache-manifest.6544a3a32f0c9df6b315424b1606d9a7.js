a739cc4a0fdde32c0fd1796a0dfcee6373f95721
Owlafiss <zdraveskia0@gmail.com>
2020-07-10 05:46:34 +0300

    Updates

---
+++ precache-manifest.6544a3a32f0c9df6b315424b1606d9a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a336e9fb26638d2a85356996feaefc5",
+    "url": "/TimeApp/index.html"
+  },
+  {
+    "revision": "3f36f9a4ad542a1cab96",
+    "url": "/TimeApp/static/css/main.c7446496.chunk.css"
+  },
+  {
+    "revision": "c73d2613471c6c736294",
+    "url": "/TimeApp/static/js/2.987c71c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TimeApp/static/js/2.987c71c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f36f9a4ad542a1cab96",
+    "url": "/TimeApp/static/js/main.1f6da3fd.chunk.js"
+  },
+  {
+    "revision": "9e1e8a8b7af79278c2ec",
+    "url": "/TimeApp/static/js/runtime-main.721fc28d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TimeApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1718216b68494ae070fb81e012470d5.js"
+  "/TimeApp/precache-manifest.6544a3a32f0c9df6b315424b1606d9a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TimeApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
